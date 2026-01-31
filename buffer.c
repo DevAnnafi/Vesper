28ec83e42aaa050e018b6b3fe53da5483bc39e92
Paradox456 <islamannafi@gmail.com>
2026-01-30 20:31:08 -0500

    Allocated initial buffer, defined gap_start and gap_end

---
+++ buffer.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "buffer.h"
+
+GapBuffer* buffer_create(size_t initial_size) {
+
+	GapBuffer *buffer = malloc(sizeof(GapBuffer));
+
+	if(buffer == NULL) {
+		return NULL;
+	}
+
+	buffer->data = malloc(sizeof(char) * initial_size);
+	if(buffer->data ==NULL) {
+		free(buffer);
+		return NULL;
+	}
+
+	buffer->gap_start = 0;
+	buffer->gap_end = initial_size;
+	buffer->size = initial_size;
+
+	return buffer;
+
+}

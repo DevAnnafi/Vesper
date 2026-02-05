93d4d376e521c904344c0e69984165fae303df77
Paradox456 <islamannafi@gmail.com>
2026-02-04 22:35:03 -0500

    feat: Implement modal editing (NORMAL and INSERT modes)
    - Added EditorMode enum with NORMAL and INSERT states
    - Added mode field to EditorState struct
    - Implemented mode-based input handling in editorLoop()
    - NORMAL mode: Commands only (h/j/k/l navigation, i to enter INSERT, q to quit)
    - INSERT mode: Character insertion, backspace deletion, ESC to return to NORMAL
    - Updated draw_status_line() to display current mode
    - Fixed horizontal scroll logic (was using row_offset instead of col_offset)
    
    Step 5 Modal Editing is now COMPLETE

--- src/editor.c
+++ src/editor.c
@@ -30,12 +30,12 @@ void scroll()
 
 	if (state.cursor_x >= state.row_offset + state.screen_cols)
 	{
-		state.col_offset = state.cursor_x = state.screen_cols + 1;
+		state.col_offset = state.cursor_x - state.screen_cols + 1;
 	}
 
 	if (state.cursor_x < state.row_offset)
 	{
-		state.row_offset = state.cursor_x;
+		state.col_offset = state.cursor_x;
 	}
 }
 
@@ -46,6 +46,7 @@ void editorLoop()
 	state.col_offset = 0;
 	state.cursor_x = 0;
 	state.cursor_y = 0;
+	state.mode = NORMAL;
 	get_terminal_size(&state.screen_rows, &state.screen_cols);
 	signal(SIGWINCH, sigwinch_handler);
 
@@ -57,7 +58,7 @@ void editorLoop()
 		printf("\x1b[H");
 
 		render_text(buffer, state.row_offset, state.screen_rows - 1, state.col_offset, state.screen_cols);
-		draw_status_line(state.cursor_x, state.cursor_y, state.screen_rows);
+		draw_status_line(state.cursor_x, state.cursor_y, state.screen_rows, state.mode);
 
 		printf("\x1b[%d;%dH", state.cursor_y + 1, state.cursor_x + 1);
 
@@ -65,42 +66,73 @@ void editorLoop()
 
 		char c;
 		read(STDIN_FILENO, &c, 1);
+
+		if (state.mode == NORMAL)
+		{
     
-		if (c == 'q') 
-		{
-			break;
-		}
-
-		else if (c == 'h') 
-		{
-			if (state.cursor_x > 0) 
-			{ 
-				state.cursor_x--;
-			}
-		}
-
-		else if (c == 'l') 
-		{
-			if (state.cursor_x < state.screen_cols - 1) 
+			if (c == 'q') 
 			{
-				state.cursor_x++;
+				break;
 			}
+
+			else if (c == 'h') 
+			{	
+				if (state.cursor_x > 0) 
+				{ 
+					state.cursor_x--;
+				}
+			}
+
+			else if (c == 'l') 
+			{
+				if (state.cursor_x < state.screen_cols - 1) 
+				{
+					state.cursor_x++;
+				}
+			}
+
+			else if (c == 'k') 
+			{	
+				if(state.cursor_y > 0) 
+				{
+					state.cursor_y--;
+				}
+			}
+
+			else if (c == 'j') 
+			{
+				if(state.cursor_y < state.screen_rows - 1) 
+				{
+					state.cursor_y++;
+
+				}
+			}
+
+		else if (c == 'i')
+		{
+			state.mode = INSERT;
 		}
 
-		else if (c == 'k') 
+	}
+
+		else if (state.mode == INSERT)
 		{
-			if(state.cursor_y > 0) 
+			// If ESC is clicked switch to NORMAL mode
+			if (c == 27)
 			{
-				state.cursor_y--;
+				state.mode = NORMAL;
 			}
-		}
 
-		else if (c == 'j') 
-		{
-			if(state.cursor_y < state.screen_rows - 1) 
+			// If BACKSPACE is clicked delete the character
+			if (c == 127)
 			{
-				state.cursor_y++;
+				buffer_delete_char(buffer);
+			}
 
+			// If Character is to be inserted insert the character
+			if (c >= 32 && c <= 126)
+			{
+				buffer_insert_char(buffer, c);
 			}
 		}
 
--- src/editor.h
+++ src/editor.h
@@ -1,6 +1,13 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+typedef enum 
+{
+	NORMAL,
+	INSERT,
+
+} EditorMode;
+
 typedef struct {
     int cursor_x;     // Cursor column position
     int cursor_y;     // Cursor row position
@@ -8,6 +15,7 @@ typedef struct {
     size_t screen_cols;  // Terminal width
     size_t row_offset; // Line in the file at the top of the screen
     size_t col_offset;
+    EditorMode mode;
 } EditorState;
 
 
